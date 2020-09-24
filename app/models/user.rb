fa2b4cd8051ad0bde5110a3e58a0dd9f35c0d4f0
Tomomi1411 <70436578+Tomomi1411@users.noreply.github.com>
2020-09-24 19:30:57 +0900

    モデルのテスト６

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+ REGIX = /\A[a-z0-9]+\z/i
+ NAMEREGIX = /\A[ぁ-んァ-ン一-龥]/
+ KANAREGIX = /\A[ァ-ヶーR－]+\z/
  with_options presence: true do
-  validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+  validates :nickname, format: { with: REGIX, message: "is invalid. Input half-width characters."}
   #validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-  validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-  validates :password_confirmation, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-  validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :surname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :surname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :password, format: { with: REGIX, message: "is invalid. Input half-width characters."}
+  validates :password_confirmation, format: { with: REGIX, message: "is invalid. Input half-width characters."}
+  validates :name, format: { with: NAMEREGIX, message: "is invalid. Input full-width characters."}
+  validates :surname, format: { with: NAMEREGIX, message: "is invalid. Input full-width characters."}
+  validates :name_kana, format: { with: KANAREGIX, message: "is invalid. Input full-width katakana characters."}
+  validates :surname_kana, format: { with: KANAREGIX, message: "is invalid. Input full-width katakana characters."}
   validates :birth
  end
 end
