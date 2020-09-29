8732a77a1a5e9af62260b7258404730fda614c82
Tomomi1411 <70436578+Tomomi1411@users.noreply.github.com>
2020-09-29 14:10:21 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
-  
+  has_one_attached :image
+
+  REGIX = /\A[ぁ-んァ-ン一-龥]/
+  validates :name, presence: true, format: { with: REGIX, message: "is invalid. Input full-width characters."}
+  validates :explanation, presence: true, format: { with: REGIX, message: "is invalid. Input full-width characters."}
+  validates :category, numericality: { other_than: 1 } 
+  validates :condition, numericality: { other_than: 1 } 
+  validates :pay_id, numericality: { other_than: 1 } 
+  validates :area_id, numericality: { other_than: 1 } 
+  validates :days_id, numericality: { other_than: 1 } 
+  validates :price, numericality: { other_than: 1 } 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "items#index"
-  resources :items
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'sending_destinations', to: 'users/registrations#newSendingDestination'
+    post 'sending_destinations', to: 'users/registrations#createSendingDestination'
+  end
+  root "items#index"
+
+  resources :items, only: [:new, :create, :update] do
+    collection do
+      get 'get_category_child', to: 'items#get_category_child', defaults: { format: 'json' }
+      get 'get_category_grandchild', to: 'items#get_category_grandchild', defaults: { format: 'json' }
+    end
+  end
 end
