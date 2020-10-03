bf3d1bc826617177f4ae738d989f3d879d9bb275
Tomomi1411 <70436578+Tomomi1411@users.noreply.github.com>
2020-10-03 21:42:06 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :create]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except:[:index]
 
   def index
-    @items = Item.all.order(id: "DESC")
-    # @items_category = Item.where("buyer_id IS NULL AND trading_status = 0 AND category_id < 200").order(created_at: "DESC")
-    # @items_brand = Item.where("buyer_id IS NULL AND  trading_status = 0 AND brand_id = 1").order(created_at: "DESC")
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
@@ -23,9 +24,33 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  private
+  def edit
+  end
+ 
+  def update
+    if @item.user_id == current_user.id
+      if @item.update(item_params)
+        redirect_to item_path(@item.id)
+      else
+        redirect_to action: :edit
+      end
+    else
+      redirect_to action: :edit
+    end
+  end
 
+  private
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :pay_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+ 
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
