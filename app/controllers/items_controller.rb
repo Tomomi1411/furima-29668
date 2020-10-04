4b42bc72339f7b7d9c5940ebbf4d1d46c13de999
Tomomi1411 <70436578+Tomomi1411@users.noreply.github.com>
2020-10-04 16:17:37 +0900

    商品削除機能完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else 
+      render :new
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
